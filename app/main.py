e8dddd5a3770c4af369dd1ead7e677a8c59ea4f6
Juan <zanon.juan@gmail.com>
2022-12-08 07:28:29 -0300

    agrego app/

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from xmlrpc.client import FastMarshaller
+from fastapi import FastAPI
+import json
+
+app=FastAPI()
+
+datos = {"1":"Python", "2":"Java", "3":"PHP","4": "Javascript","5":"Panchi"}
+
+@app.get("/")
+def raiz():
+    sin_codificar = json.dumps(datos)
+    return json.loads(sin_codificar)
+
+@app.get("/validar/{numero}")
+def validar_capicua (numero:str):
+    respuesta = 'No es capicua'
+    if numero == numero[::-1]:
+        respuesta = 'Es capicua'
+    
+    return {"numero": numero,
+            "validacion": respuesta}
+
+@app.post("/students")
+def guardar(name:str, lastname:str):
+    return f"Estudiante {name} {lastname} guardado!"
